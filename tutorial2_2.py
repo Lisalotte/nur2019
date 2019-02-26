548b2b8e679318289b916d520e542a50641c6d4b
Lisalotte <lisapothoven@gmail.com>
2019-02-26 13:32:08 +0100

    tutorial 2

--- tutorial2_2.py
+++ tutorial2_2.py
@@ -27,10 +27,21 @@ def GaussJordanElimination(matrix,c,r):
 	for j in range(0,c): #columns
 		for i in range(0,r): #rows	
 			if ((i != j) and (j < 5)):
-				print(i,j)
 				if (matrix[i][j] != 0):	
 					matrix[i] = matrix[j][j]*matrix[i] - matrix[i][j]*matrix[j]
 	return matrix
+
+def solveMatrix(matrix,c,r,x,y_array):
+	my_value = matrix[r][c]
+	y = 0
+	if (c < 4 and r < 4):
+		y += matrix[r][c+1] * solveMatrix(matrix,c+1,r+1,y,y_array)
+	if(my_value != 0):
+		y = matrix[r][-1]/my_value
+	y_array.append(y)
+	print(y)
+	return x
+
 
 def main():
 	matrix = np.array([
@@ -39,7 +50,7 @@ def main():
 		[4, 4, 3, -40, -3, 1],
 		[0, 2, 1, -3, -2, 0],
 		[0, 1, 0, -12, -0, 0]
-	])
+	],np.float)
 
 	c = 6
 	r = 5
@@ -53,7 +64,21 @@ def main():
 	print(matrix)
 	print(matrix_Gauss)
 	print(matrix_Jordan)
-	print(GaussJordanElimination(matrix_Gauss,c,r))
+#		print(GaussJordanElimination(matrix_Jordan,c,r))
+	x = 0
+	y_array = []
+	solveMatrix(matrix_Gauss,0,0,x,y_array)
+	print(y_array)
+#	y_array = []
+#	solveMatrix(matrix_Jordan,0,0,x,y_array)
+#	print(y_array)
+
+	c = 0
+	val = 0
+	for y in y_array:
+		val += y*matrix[0][c]
+		c += 1
+	print(val)
 
 main()
 				
